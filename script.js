471bce2abe83db5bd39b89e70ca39f864d5d7aed
Hannah Ebbinghaus <hannah.ebb.he@gmail.com>
2022-04-13 14:22:29 -0300

    estrutura_html_css_pronta

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ESTRUTURAS EM HTML A SEREM ADICIONADAS COMO EVENTOS DE ENTRADA OU MENSAGEM:
+
+//<div class="message"> <h4>"hora"</h4> <h3>"mensagem aqui"</h3> </div>
+//<div class="message private"> <h4>"hora"</h4> <h3>"mensagem aqui"</h3> </div>
+//<div class="message enteredchat"> <h4>"hora"</h4> <h3>"mensagem aqui"</h3> </div>
