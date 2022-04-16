2dd9795eefa7f40c0da43ffc070425f2566de8a5
Hannah Ebbinghaus <hannah.ebb.he@gmail.com>
2022-04-16 20:53:13 -0300

    send_messages_to_go

--- script.js
+++ script.js
@@ -1,77 +1,80 @@
 
 //LINK DA PÁGINA COM INFORMAÇÕES DO API DO PROJETO: https://bootcampra.notion.site/API-Chat-UOL-1df39c2a8d75450f9b82920163d306b0
 
+//VARIÁVEIS GLOBAIS
 
+const display = document.querySelector(".container");
+display.scrollIntoView();
 
-// PARTE 01 - REQUISIÇÃO ENVIAR DADOS AO SERVIDOR - entrar na sala
-
+userName = "";
 let users = [];
+let messages = [];
 
-enterRoom ()
+//CHAMAR FUNÇÕES + PROMPT
+setInterval(getData (), 3000)
+setInterval(enterRoom (), 5000)
+userName = prompt("Qual o seu nome?");
 
-function getData () {
-  const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
-  promise.then(chargeData);
-  console.log(promise)
-}
+
+// PARTE 01 - ENTRAR NA SALA - POST
 
 function enterRoom () {
+  
+    const enteredRoom = {
+      name: userName
+    }; 
+  
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', enteredRoom);
+  
+    promise.then(getData);
+  
+    //promise01.catch(tratarError);
+  //O servidor pode responder com status 400 se já houver um usuário online com esse nome. Se for o caso, a aplicação deve pedir um novo nome até que o servidor responda com status 200.
+  }
 
-  const enteredRoom = {
-    name: "Céline Dion"
-  }; 
+  function getData () {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(chargeData);
+    console.log(promise)
+  }
 
-  const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', enteredRoom);
-
-  promise.then(getData);
-
-  //promise01.catch(tratarError);
-//O servidor pode responder com status 400 se já houver um usuário online com esse nome. Se for o caso, a aplicação deve pedir um novo nome até que o servidor responda com status 200.
-}
-
-function chargeData (response) {
-  // Se a promise for bem sucedida, chama "carregarDados", que faz um get para o servidor para pegar o nome dos participantes do chat. Se tiver response positivo, chama a função displayName, para jogar a div na tela (com o scroll)
-  users = response.data;
-  displayName();
-  console.log(users)
-
-}
+  function chargeData (response) {
+    // Se a promise for bem sucedida, chama "carregarDados", que faz um get para o servidor para pegar o nome dos participantes do chat. Se tiver response positivo, chama a função displayName, para jogar a div na tela (com o scroll)
+    messages = response.data;
+    displayMessages();
+  
+  }
+  
 
 
-function displayName () {
-  const display = document.querySelector(".container");
-  display.scrollIntoView();
-  let date = Date();
 
-  for(let i = 0; i < users.length; i++) {
-    display.innerHTML += `
-    <div class="message enteredchat"> <h4>${date.slice(16, 24)}</h4> <h3> <strong>${users[i].name}</strong> entrou na sala </h3> </div>
-   `;
+// PARTE 02 - CARREGAR MENSAGENS QUE JÁ ESTÃO NO SERVIDOR A CADA 3 SEGUNDOS - assim que usuário entra na página
+
+
+function displayMessages () {
+  for(i = 0; i < messages.length; i++) {
+if (messages[i].to === "Todos" && messages[i].type === "message") {
+
+  display.innerHTML += `
+  <div class="message"> <h4>${messages[i].time}</h4> <h3> <strong>${messages[i].from}</strong> ${messages[i].text}</h3> </div>
+ `;
+
+} if (messages[i].to === userName && messages[i].type === "message") {
+
+  display.innerHTML += `
+  <div class="message private"> <h4>${messages[i].time}</h4> <h3> <strong>${messages[i].from}</strong> para <strong>${messages[i].to}</strong> ${messages[i].text}</h3> </div>
+ `;
+
+} else if (messages[i].type === "status") {
+  display.innerHTML += `
+  <div class="message enteredchat"> <h4>${messages[i].time}</h4> <h3> <strong>${messages[i].from}</strong> ${messages[i].text} </h3> </div>
+ `;
+} 
 
   }
 
-  
 }
 
-
-
-
-
-
-//PARTE 02 - REQUISIÇÃO ENVIAR DADOS AO SERVIDOR - manter conectado
-//const dados = {...}; //name?
-//const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', dados);
-
-//requisicao.then(tratarSucesso);
-//requisicao.catch(tratarError);
-//Enviando um objeto no formato enviando o nome do usuário que foi pedido ao entrar na página. Esta requisição deve ser feita a cada 5s.
-
-
-
-
-//REQUISIÇÃO DE INFORMAÇÃO DO SERVIDOR - buscar mensagens no servidor
-//const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-//promise.then(processarResposta);
 
 //const promessa = axios.get('http://...');
 
@@ -84,6 +87,25 @@ function displayName () {
 //  console.log("Status code: " + erro.response.status); // Ex: 404
 //	console.log("Mensagem de erro: " + erro.response.data); // Ex: Not Found
 //}
+
+
+
+
+
+
+
+//PARTE XX - REQUISIÇÃO ENVIAR DADOS AO SERVIDOR - manter conectado
+//const dados = {...}; //name?
+//const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', dados);
+
+//requisicao.then(tratarSucesso);
+//requisicao.catch(tratarError);
+//Enviando um objeto no formato enviando o nome do usuário que foi pedido ao entrar na página. Esta requisição deve ser feita a cada 5s.
+
+
+
+
+
 
 
 
